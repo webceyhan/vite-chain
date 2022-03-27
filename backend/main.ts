36011b5e1f881685aa116ca52244946f445758df
Evren Ceyhan <evren@ceyhan.io>
2022-03-28 00:23:35 +0300

    minimal blockchain objects created

--- backend/main.ts
+++ backend/main.ts
@@ -1,1 +1,28 @@
-console.log('backend');
+export class Transaction {
+    constructor(
+        public from: string,
+        public to: string,
+        public amount: number,
+        public timestamp: number = Date.now(),
+        public hash: string = ''
+    ) {}
+}
+
+export class Block {
+    constructor(
+        public index: number,
+        public parentHash: string,
+        public transactions: Transaction[],
+        public timestamp: number = Date.now(),
+        public hash: string = ''
+    ) {}
+}
+
+export class BlockChain {
+    constructor(public chain: Block[]) {}
+}
+
+const genesisBlock = new Block(0, '', [], 1545184500, '0');
+const blockChain = new BlockChain([genesisBlock]);
+
+console.log(blockChain);
