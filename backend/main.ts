9289aa729b099f320fd474c865e8a03d85e25afe
Evren Ceyhan <evren@ceyhan.io>
2022-03-28 00:29:27 +0300

    genesis transaction added

--- backend/main.ts
+++ backend/main.ts
@@ -1,7 +1,9 @@
 import { Block } from './Block';
 import { BlockChain } from './BlockChain';
+import { Transaction } from './Transaction';
 
-const genesisBlock = new Block(0, '', [], 1545184500, '0');
+const genesisTx = new Transaction('0', 'miner', 100);
+const genesisBlock = new Block(0, '', [genesisTx], 1545184500, '0');
 const blockChain = new BlockChain([genesisBlock]);
 
 console.log(blockChain);
