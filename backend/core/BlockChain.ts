80db27d4dae61ec9d7b60c5486dec0897ce86609
Evren Ceyhan <evren@ceyhan.io>
2022-03-28 23:53:29 +0300

    pending transactions added

--- backend/core/BlockChain.ts
+++ backend/core/BlockChain.ts
@@ -1,10 +1,22 @@
 import { Block, GENESIS_BLOCK } from './Block';
+import { Transaction } from './Transaction';
 
 export class BlockChain {
-    constructor(
-        /**
-         * The chain of blocks starting from the genesis block.
-         */
-        public chain: Block[] = [GENESIS_BLOCK]
-    ) {}
+    /**
+     * The chain of blocks starting from the genesis block.
+     */
+    public chain: Block[] = [GENESIS_BLOCK];
+
+    /**
+     * Pending transactions to be added to the next block.
+     */
+    private pendingTransactions: Transaction[] = [];
+
+    /**
+     * Add new transaction to the blockchain.
+     */
+    addTransaction(tx: Transaction): void {
+        // todo: validate transaction
+        this.pendingTransactions.push(tx);
+    }
 }
