1c6cbc81c129ee3b2e7b5cb010c0e9b81908f047
Evren Ceyhan <evren@ceyhan.io>
2022-03-28 21:37:08 +0300

    transaction docs updated

--- backend/Transaction.ts
+++ backend/Transaction.ts
@@ -2,15 +2,38 @@ import { createHash } from './utils';
 
 export class Transaction {
     constructor(
+        /**
+         * Sender address of the transaction.
+         */
         public from: string,
+
+        /**
+         * Recipient address of the transaction.
+         */
         public to: string,
+
+        /**
+         * Amount of the transaction.
+         */
         public amount: number,
+
+        /**
+         * Timestamp on which the transaction was created.
+         */
         public timestamp: number = Date.now(),
+
+        /**
+         * Calculated hash of the transaction.
+         */
         public hash: string = ''
     ) {
+        // set transaction hash
         this.hash = this.calculateHash();
     }
 
+    /**
+     * Calculate the hash of the transaction.
+     */
     calculateHash(): string {
         return createHash(this.from + this.to + this.amount + this.timestamp);
     }
