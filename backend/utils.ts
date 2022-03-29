28c8d7e4a6a716b945eae9daba068639b43f8cf8
Evren Ceyhan <evren@ceyhan.io>
2022-03-29 23:55:44 +0300

    delay util added

--- backend/utils.ts
+++ backend/utils.ts
@@ -1,6 +1,12 @@
 import { createHash as _createHash } from 'crypto';
 
 /**
+ * Get delay in seconds.
+ */
+export const delay = (second: number) =>
+    new Promise<void>((resolve) => setTimeout(resolve, second * 1000));
+
+/**
  * Generate a sha256 hash from a string.
  * Result is a hexadecimal string with length 64.
  */
