2b9bacaa37f2e604543f8215e86cc3c2443db753
Evren Ceyhan <evren@ceyhan.io>
2022-03-30 22:12:14 +0300

    pub-key pair helpers added

--- backend/utils.ts
+++ backend/utils.ts
@@ -1,4 +1,5 @@
 import { createHash as _createHash } from 'crypto';
+import { ec } from 'elliptic';
 
 /**
  * Get delay in seconds.
@@ -12,3 +13,50 @@ export const delay = (second: number) =>
  */
 export const createHash = (str: string) =>
     _createHash('sha256').update(str).digest('hex');
+
+// KEY-PAIR HELPERS ////////////////////////////////////////////////////////////////////////////////
+
+// define the elliptic curve
+const EC = new ec('secp256k1');
+
+// define key-pair object type
+export interface KeyPair extends ec.KeyPair {}
+
+/**
+ * Create new key-pair object from private key in hex string.
+ */
+export const keyFromPrivate = (key: string): KeyPair =>
+    EC.keyFromPrivate(key, 'hex');
+
+/**
+ * Create new private / public key-pair object.
+ *
+ * To convert private key to hexadecimal string use:
+ *  - keyPair.getPrivateKey('hex')
+ *
+ * To convert public key to compact hexadecimal string use:
+ *  - keyPair.getPublicKey(true, 'hex')
+ */
+export const createKeyPair = (): KeyPair => EC.genKeyPair();
+
+/**
+ * Create a signature from a private key and data.
+ */
+export const createSignature = (key: string, data: string): string =>
+    EC.keyFromPrivate(key, 'hex').sign(data).toDER('hex');
+
+/**
+ * Verify a signture of data with corresponding public key.
+ */
+export const verifySignature = (
+    key: string,
+    data: string,
+    signature: string
+): boolean => {
+    try {
+        return EC.keyFromPublic(key, 'hex').verify(data, signature);
+    } catch (error) {
+        // ignore errors
+        return false;
+    }
+};
