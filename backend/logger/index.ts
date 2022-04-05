a0290bd1ffcb5549d0d394a4d3369bb4fc477048
Evren Ceyhan <evren@ceyhan.io>
2022-04-06 00:28:55 +0300

    chain event impl. in logger

--- backend/logger/index.ts
+++ backend/logger/index.ts
@@ -1,4 +1,5 @@
 import { Chain } from '../core';
+import { formatCoin } from './format';
 import { log, info } from './log';
 
 export const createLogger = (chain: Chain) => {
@@ -9,6 +10,30 @@ export const createLogger = (chain: Chain) => {
     // log genesis block
     info('Genesis block initialized');
     log(chain.genesisBlock);
+
+    // log mined block
+    chain.on('block:mined', (block) => {
+        info('New block mined.');
+        log(block);
+    });
+
+    // log new pending transaction
+    chain.on('transaction:added', (tx) => {
+        info('New pending transaction added.');
+        log(tx);
+    });
+
+    // log discarded transaction
+    chain.on('transaction:discarded', (tx) => {
+        info('Pending transaction discarded.');
+        log(tx);
+    });
+
+    // log supply changes
+    chain.on('supply:changed', (pool) => {
+        info(`Total Supply changed: ${formatCoin(pool.total)}`);
+        log(pool);
+    });
 
     return { log, info };
 };
--- backend/main.ts
+++ backend/main.ts
@@ -1,9 +1,13 @@
 import { Chain, Wallet } from './core';
+import { createLogger } from './logger';
 
 // create miner wallet
 export const miner = new Wallet();
 
 // create blockchain instance
 export const chain = new Chain(miner.address);
+
+// create logger for chain events
+export const logger = createLogger(chain);
 
 chain.startMiningLoop();
