3539316b56db82502e76580d72c9fd74cde56f8c
Evren Ceyhan <evren@ceyhan.io>
2022-04-06 00:31:18 +0300

    random helper added

--- backend/utils.ts
+++ backend/utils.ts
@@ -9,6 +9,12 @@ export const compare = (a: any, b: any) =>
     JSON.stringify(a) === JSON.stringify(b);
 
 /**
+ * Get a random number between min and max.
+ */
+export const random = (max: number, min: number = 0) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+
+/**
  * Get delay in seconds.
  */
 export const delay = (second: number) =>
