918cffeb7d8760c57990adb04dcdbd88387c293f
Evren Ceyhan <evren@ceyhan.io>
2022-04-06 21:51:25 +0300

    common util helpers added

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+import { format as timeAgoFormatter } from 'timeago.js';
+
+export const formatDate = (date: any) => new Date(date).toLocaleString();
+
+export const formatTimeAgo = (date: any) => timeAgoFormatter(date);
+
+export const formatCurrency = (value: number) =>
+    new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD' })
+        .format(value)
+        .replace(/\$/, '')
+        .concat(` VTC`);
