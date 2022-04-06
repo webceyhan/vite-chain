72d87a53c432cfbc25268d116d1255a6b70842ff
Evren Ceyhan <evren@ceyhan.io>
2022-04-06 21:37:42 +0300

    app layout with navigation impl.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
 
+import Navigation from './components/Navigation.vue';
+
 </script>
 
 <template>
-  <h1>ViteChain</h1>
+  <Navigation class="fixed-top" />
+
+  <div class="container my-5">
+    <router-view></router-view>
+  </div>
 </template>
+
+
+<style>
+.list-group-item a {
+  text-decoration: none;
+}
+.list-group-item a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+
+import Icon from './common/Icon.vue';
+
+const links = [
+    {
+        name: 'Blocks',
+        path: '/blocks',
+        icon: 'block'
+    },
+    {
+        name: 'Transactions',
+        path: '/transactions',
+        icon: 'tx'
+    },
+    {
+        name: 'Wallets',
+        path: '/wallets',
+        icon: 'wallet'
+    },
+];
+
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container">
+            <!-- brand link -->
+            <router-link class="navbar-brand" to="/">ViteChain</router-link>
+
+            <!-- toggle button -->
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- collapsible content -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-md-auto">
+                    <li class="nav-item d-flex" v-for="(link, i) in links" :key="i">
+                        <span class="vr mx-2"></span>
+                        <router-link class="nav-link" :to="link.path">
+                            <Icon :name="link.icon" class="me-1" />
+                            {{ link.name }}
+                        </router-link>
+                    </li>
+                </ul>
+
+                <!-- my wallet -->
+                <ul class="navbar-nav">
+                    <li class="nav-item d-flex">
+                        <span class="d-md-none vr mx-2"></span>
+                        <router-link class="nav-link" to="/my-wallet">
+                            <Icon name="wallet" class="me-1" />
+                            My Wallet
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.navbar-dark {
+    background-color: #11314d !important;
+}
+</style>
