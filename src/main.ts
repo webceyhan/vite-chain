1b403bf305ff9b8a5c139f646b1d1be8f9c6581c
Evren Ceyhan <evren@ceyhan.io>
2022-04-06 21:33:41 +0300

    bootstrap loaded

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+createApp(App).mount('#app');
