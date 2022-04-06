744a46fdd58e2e5f36f7d363d36ff1a872b7f440
Evren Ceyhan <evren@ceyhan.io>
2022-04-06 16:11:41 +0300

    api routes refactored

--- backend/api/routes/blocks.ts
+++ backend/api/routes/blocks.ts
@@ -5,15 +5,26 @@ export default (chain: Chain) => {
     // define router
     const router = Router();
 
-    // get all blocks
+    /**
+     * Get all blocks.
+     */
     router.get('/', (req, res) => {
         res.json(chain.blocks);
     });
 
-    // get block by height
+    /**
+     * Get block by height.
+     */
     router.get('/:height', (req, res) => {
+        // parse height param
         const height = +req.params.height;
-        res.json(chain.findBlockByHeight(height));
+
+        try {
+            // try to find block by height
+            res.json(chain.findBlockByHeight(height));
+        } catch (error) {
+            res.status(404).json((error as Error).message);
+        }
     });
 
     return router;
--- backend/api/routes/transactions.ts
+++ backend/api/routes/transactions.ts
@@ -5,33 +5,53 @@ export default (chain: Chain) => {
     // define router
     const router = Router();
 
-    // get all transactions
+    /**
+     * Get all transactions.
+     */
     router.get('/', (req, res) => {
-        // get by block height
-        if (req.query.block) {
-            const height = +req.query.block;
-            const block = chain.findBlockByHeight(height);
+        try {
+            // get by block height
+            if (req.query.block) {
+                // parse height param
+                const height = +req.query.block;
 
-            return res.json(block.transactions);
+                // try to find block by height
+                const block = chain.findBlockByHeight(height);
+
+                return res.json(block.transactions);
+            }
+
+            // get by address
+            if (req.query.address) {
+                // parse address param
+                const address = req.query.address as string;
+
+                // try to find all transactions by address
+                const transactions = chain.findTransactionsByAddress(address);
+
+                return res.json(transactions);
+            }
+
+            // return all transactions from last block
+            res.json(chain.lastBlock.transactions);
+        } catch (error) {
+            res.status(404).json((error as Error).message);
         }
-
-        // get by address
-        if (req.query.address) {
-            const address = req.query.address as string;
-            const transactions = chain.findTransactionsByAddress(address);
-
-            return res.json(transactions);
-        }
-
-        // return all transactions from last block
-        res.json(chain.lastBlock.transactions);
     });
 
-    // get transaction by hash
+    /**
+     * Get transaction by hash.
+     */
     router.get('/:hash', (req, res) => {
+        // parse hash param
         const hash = req.params.hash;
 
-        res.json(chain.findTransaction(hash));
+        try {
+            // try to find transaction by hash
+            res.json(chain.findTransaction(hash));
+        } catch (error) {
+            res.status(404).json((error as Error).message);
+        }
     });
 
     return router;
