e54a0e35cf9066b3dda9cef73f2b2730f7e0a641
Evren Ceyhan <evren@ceyhan.io>
2022-04-12 16:32:42 +0300

    demo wallet added

--- backend/test.ts
+++ backend/test.ts
@@ -2,6 +2,14 @@ import { chain, miner, logger } from './main';
 import { delay, random } from './utils';
 import { Wallet } from './core';
 
+// DEMO WALLET
+// private  : 633d6bee776c7b54359bf56c5c0767f859c49345f9dac5aef5fd8d1d1a0bb616
+// public   : 02f3566c7ba66b952035fc91f853eaabaee95afede0a15993e28482a64efb45ef3
+// address  : 6jf5w4zadRti1C3NVqvHoYrZwjA9bTGyunxrzHsXkEJyyczfxb
+const demoWallet = Wallet.fromKey(
+    '633d6bee776c7b54359bf56c5c0767f859c49345f9dac5aef5fd8d1d1a0bb616'
+);
+
 // fake user wallet pool
 const wallets: Wallet[] = [];
 
@@ -12,7 +20,8 @@ const populateWallets = async (count = 5) => {
         wallets.push(new Wallet());
     }
 
-    console.log(`Total ${wallets.length} wallets created.\n`);
+    // add demo wallet to pool
+    wallets.push(demoWallet);
 };
 
 // get a random wallet
