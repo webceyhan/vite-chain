efcc3aec8725547d5bc48a8287f9c0df45e7f552
Evren Ceyhan <evren@ceyhan.io>
2022-04-16 13:03:17 +0300

    chain stat placeholders added

--- src/components/ChainStats.vue
+++ src/components/ChainStats.vue
@@ -1,59 +1,72 @@
 <script setup lang="ts">
-
-import { PropType } from 'vue';
-import ListGroup from './common/ListGroup.vue';
-import ListGroupItem from './common/ListGroupItem.vue';
-import Timestamp from './common/Timestamp.vue';
-import Icon from './common/Icon.vue';
+import { PropType } from "vue";
+import ListGroup from "./common/ListGroup.vue";
+import ListGroupItem from "./common/ListGroupItem.vue";
+import Timestamp from "./common/Timestamp.vue";
+import Placeholder from "./common/Placeholder.vue";
+import Icon from "./common/Icon.vue";
 
 defineProps({
-    stats: { type: Object as PropType<any>, default: () => { } },
-})
-
+  stats: { type: Object as PropType<any>, default: () => {} },
+});
 </script>
 
 <template>
-    <ListGroup class="list-group-horizontal-lg">
-        <ListGroupItem class="flex-fill p-lg-4">
-            <div class="col-auto align-self-start">
-                <Icon name="time" class="h1" />
-            </div>
-            <div class="col">
-                <small class="text-muted">Time of last block</small>
-                <div class="display-6">
-                    <Timestamp :value="stats.lastBlockTimestamp" />
-                </div>
-            </div>
-        </ListGroupItem>
+  <ListGroup class="list-group-horizontal-lg">
+    <ListGroupItem class="flex-fill p-lg-4">
+      <div class="col-auto align-self-start">
+        <Icon name="time" class="h1" />
+      </div>
+      <div class="col">
+        <small class="text-muted">Time of last block</small>
+        <Placeholder :value="stats.lastBlockTimestamp">
+          <div class="display-6">
+            <Timestamp :value="stats.lastBlockTimestamp" />
+          </div>
+        </Placeholder>
+      </div>
+    </ListGroupItem>
 
-        <ListGroupItem class="flex-fill p-lg-4">
-            <div class="col-auto align-self-start">
-                <Icon name="block" class="h1" />
-            </div>
-            <div class="col">
-                <small class="text-muted">Total Blocks</small>
-                <div class="display-6">{{ stats.totalBlocks }}</div>
-            </div>
-        </ListGroupItem>
+    <ListGroupItem class="flex-fill p-lg-4">
+      <div class="col-auto align-self-start">
+        <Icon name="block" class="h1" />
+      </div>
+      <div class="col">
+        <small class="text-muted">Total Blocks</small>
+        <Placeholder :value="stats.totalBlocks">
+          <div class="display-6">
+            <span class="h1">{{ stats.totalBlocks }}</span>
+          </div>
+        </Placeholder>
+      </div>
+    </ListGroupItem>
 
-        <ListGroupItem class="flex-fill p-lg-4">
-            <div class="col-auto align-self-start">
-                <Icon name="tx" class="h1" />
-            </div>
-            <div class="col">
-                <small class="text-muted">Total Transactions</small>
-                <div class="display-6">{{ stats.totalTransactions }}</div>
-            </div>
-        </ListGroupItem>
+    <ListGroupItem class="flex-fill p-lg-4">
+      <div class="col-auto align-self-start">
+        <Icon name="tx" class="h1" />
+      </div>
+      <div class="col">
+        <small class="text-muted">Total Transactions</small>
+        <Placeholder :value="stats.totalTransactions">
+          <div class="display-6">
+            <span class="h1">{{ stats.totalTransactions }}</span>
+          </div>
+        </Placeholder>
+      </div>
+    </ListGroupItem>
 
-        <ListGroupItem class="flex-fill p-lg-4">
-            <div class="col-auto align-self-start">
-                <Icon name="wallet" class="h1" />
-            </div>
-            <div class="col">
-                <small class="text-muted">Total Wallets</small>
-                <div class="display-6">{{ stats.totalWallets }}</div>
-            </div>
-        </ListGroupItem>
-    </ListGroup>
+    <ListGroupItem class="flex-fill p-lg-4">
+      <div class="col-auto align-self-start">
+        <Icon name="wallet" class="h1" />
+      </div>
+      <div class="col">
+        <small class="text-muted">Total Wallets</small>
+        <Placeholder :value="stats.totalWallets">
+          <div class="display-6">
+            <span class="h1">{{ stats.totalWallets }}</span>
+          </div>
+        </Placeholder>
+      </div>
+    </ListGroupItem>
+  </ListGroup>
 </template>
---
+++ src/components/common/Placeholder.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ value?: any }>();
+</script>
+
+<template>
+  <slot v-if="value !== undefined" />
+
+  <p v-else class="placeholder-glow">
+    <span class="placeholder placeholder-xs bg-secondary col-12"></span>
+  </p>
+</template>
+
