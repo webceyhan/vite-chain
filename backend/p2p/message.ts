872239f3340f64165d7b269145af528bbe00224d
Evren Ceyhan <evren@ceyhan.io>
2022-04-19 22:12:14 +0300

    p2p messages refactored

--- backend/p2p/message.ts
+++ backend/p2p/message.ts
@@ -1,18 +1,13 @@
 import { RawData } from 'ws';
-import { Node } from '../node';
 
 export type MessageName =
     | 'newTransaction'
     | 'newBlock'
-    //
-    | 'queryChainSize'
     | 'queryChain'
-    | 'queryLastBlock'
-    | 'queryTransactions'
-    //
-    | 'responseChainSize'
     | 'responseChain'
+    | 'queryLastBlock'
     | 'responseLastBlock'
+    | 'queryTransactions'
     | 'responseTransactions';
 
 export type Message = {
@@ -33,46 +28,3 @@ export const parseMessage = (raw: RawData): Message =>
  */
 export const serializeMessage = (message: Message): string =>
     JSON.stringify(message);
-
-const createMessage = (name: MessageName, data?: any): Message => ({
-    name,
-    data,
-});
-
-export const useMessages = (node: Node) => {
-    // chain event messages
-    const newTransaction = (tx: any) => createMessage('newTransaction', tx);
-    const newBlock = (block: any) => createMessage('newBlock', block);
-
-    // define query messages
-    const queryChain = () => createMessage('queryChain');
-    const queryChainSize = () => createMessage('queryChainSize');
-    const queryLastBlock = () => createMessage('queryLastBlock');
-    const queryTransactions = () => createMessage('queryTransactions');
-
-    // define response messages
-    const responseChainSize = () =>
-        createMessage('responseChainSize', node.chainSize);
-
-    const responseChain = () => createMessage('responseChain', node.blocks);
-
-    const responseLastBlock = () =>
-        createMessage('responseLastBlock', node.lastBlock);
-
-    const responseTransactions = () =>
-        createMessage('responseTransactions', node.pendingTransactions);
-
-    return {
-        newTransaction,
-        newBlock,
-        //
-        queryChainSize,
-        queryChain,
-        queryLastBlock,
-        queryTransactions,
-        responseChainSize,
-        responseChain,
-        responseLastBlock,
-        responseTransactions,
-    };
-};
